3a1e7409496cf805f641cb97db26e4b22e8d83aa
andrew12314 <33496160+andrew12314@users.noreply.github.com>
2017-11-08 18:58:32 +0000

    loadingscreen

---
+++ js/config.js
@@ -0,0 +1,127 @@
+﻿/*    _           _    _                     _ 
+     | |         | |  | |                   | |
+  ___| | ___  ___| | _| |     ___   __ _  __| |
+ / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
+ \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
+ |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
+    version 2.05
+*/
+
+/*  Leaked by ♏α⊥⊥ḯṧḯṧм  */
+
+/*URL for the server logo
+Leave it blank to use text instead*/
+var l_serverImage = "images/logo.png";
+
+/*Custom server name configuration
+Leave it blank for automatic server name*/
+var l_serverName = "Xanath Roleplay";
+
+/*Use a video for the background?
+Otherwise it will just use images*/
+var l_bgVideo = false;
+
+/*Video background
+Avaiable options:
+"backgrounds/videos/aurora.webm"
+"backgrounds/videos/bluefall.webm"
+"backgrounds/videos/bluetilefloor.webm"
+"backgrounds/videos/bluewhitecircles.webm"
+"backgrounds/videos/bokehlens.webm"
+"backgrounds/videos/cleanbokeh.webm"
+"backgrounds/videos/coldnight.webm"
+"backgrounds/videos/colorfulburst.webm"
+"backgrounds/videos/dancingstars.webm"
+"backgrounds/videos/dropsflares.webm"
+"backgrounds/videos/focusorangebokeh.webm"
+"backgrounds/videos/glimmering.webm"
+"backgrounds/videos/glowingrings.webm"
+"backgrounds/videos/greencircles.webm"
+"backgrounds/videos/hues.webm"
+"backgrounds/videos/inthedeep.webm"
+"backgrounds/videos/modernflourish.webm"
+"backgrounds/videos/orangeaura.webm"
+"backgrounds/videos/warminside.webm"
+"backgrounds/videos/yellowgreen.webm"
+
+You can add custom backgrounds
+Just make sure it is at webm VP8 format */
+var l_background = "";
+
+/*(Image-background only)
+Place one or more images
+Multiple images will automatically shuffle between them*/
+var l_bgImages = [
+	"backgrounds/images/rebel.jpg",
+	"backgrounds/images/darkrp.png",
+	"backgrounds/images/preview.jpg",
+        "backgrounds/images/maxresdefault.jpg",
+        "backgrounds/images/picture.jpg",
+        "backgrounds/images/nonce.jpg",
+        "backgrounds/images/overwatch.jpg",
+        "backgrounds/images/soldierbeinganonce.jpg",
+
+];
+
+/*Random background images order?*/
+var l_bgImagesRandom = true;
+
+/*(Image-background only)
+Delay between background images changes
+in milliseconds*/
+var l_bgImageDuration = 3500;
+
+/*(Image-background only)
+Image background fade velocity
+in milliseconds*/
+var l_bgImageFadeVelocity = 2000;
+
+/*Enable background overlay?*/
+var l_bgOverlay = true;
+
+/*Background darkening amount
+0 for none (0%), 100 for pitch black (100%)*/
+var l_bgDarkening = 50;
+
+/*Music player
+'true' or 'false' to enable music in the background*/
+var l_music = false;
+
+/*Display actual song's name?*/
+var l_musicDisplay = false;
+
+/*Music playlist
+Place how much Youtube IDs/.ogg you want*/
+var l_musicPlaylist = [
+	{youtube: "ID", name: "Song name"},
+    {youtube: "ID", name: "Song name"},
+	{youtube: "ID", name: "Song name"}
+];
+
+/*Random music order?*/
+var l_musicRandom = true;
+
+/*Music volume
+Choose a value between 0 and 100*/
+var l_musicVolume = 15;
+
+/*Enable custom messages?*/
+var l_messagesEnabled = true;
+
+/*Enter your custom messages below*/
+var l_messages = [
+"Enjoy the server",
+"Thanks for joining us!",
+"Establised November 2015"
+];
+
+/*Random message order?*/
+var l_messagesRandom = true;
+
+/*Delay between message changes
+in milliseconds*/
+var l_messagesDelay = 5000;
+
+/*Messages fade time
+in milliseconds*/
+var l_messagesFade = 1000;
